792059e23b990baee48b700ce647fccd2f445358
henrionj <julien.henrion@iut-valence.fr>
2016-02-17 16:53:51 +0100

    beginning of the Chess Project v0.0

---
+++ src/chess/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package chess;
+
+/**
+ * @author henrion & murer
+ *
+ */
+public class Main
+{
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
