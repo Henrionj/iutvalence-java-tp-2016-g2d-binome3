6880a6db70e43e9cb98249c548132a9c3c0177c6
henrion julien <julien.henrion@iut-valence.fr>
2016-03-02 17:31:35 +0100

    adding attribute to piece

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -16,4 +16,25 @@ public class Piece
 	{
 		
 	}
+	
+	public int getPosX()
+	{
+		return posX;
+	}
+	
+	public void setPosX(int posX)
+	{
+		this.posX = posX;
+	}
+	
+	public int getPosY()
+	{
+		return posY;
+	}
+	
+	public void setPosY(int posY)
+	{
+		this.posY = posY;
+	}
+	
 }
