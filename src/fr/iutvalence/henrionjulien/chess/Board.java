52d1bcd82bbaf464347dc22987baf6d1c9cdbfa4
henrion julien <julien.henrion@iut-valence.fr>
2016-03-16 16:20:08 +0100

    add a new type of Piece, Blank to simulate a blank case. changing the createPieceGame() method of Board, removing the two boards of pieces for create a unique board of pieces.

--- src/fr/iutvalence/henrionjulien/chess/Board.java
+++ src/fr/iutvalence/henrionjulien/chess/Board.java
@@ -1,6 +1,7 @@
 package fr.iutvalence.henrionjulien.chess;
 
 import fr.iutvalence.henrionjulien.chess.piece.Bishop;
+import fr.iutvalence.henrionjulien.chess.piece.Blank;
 import fr.iutvalence.henrionjulien.chess.piece.King;
 import fr.iutvalence.henrionjulien.chess.piece.Knight;
 import fr.iutvalence.henrionjulien.chess.piece.Pawn;
@@ -20,44 +21,15 @@ public class Board
 	public static final int WIDTH  = 8;
 	/** Chessboard's height. */
 	public static final int HEIGHT = 8;
-	/** Number of pieces per player. **/
-	public static final int NUMBER_OF_PIECES = 16;
-	/** All the black pieces. */
-	/* TODO final? */
-	private Piece black[];
-	/** All the white pieces. */
-	/* TODO final? */
-	private Piece white[];
 
+	/** the board of pieces of the game*/
+	private Piece pieces[][];
 
 	/** TODO. */
 	public Board()
 	{
-		white = createPieceGame();
-		black = createPieceGame();
-	}
+		pieces = createPieceGame();
 
-	/**
-	 * TODO.
-     *
-	 * @param indice TODO
-	 * @return TOOD
-	 */
-
-	public Piece getBlackPiece(int indice)
-	{
-		return black[indice];
-	}
-
-	/**
-	 * TODO.
-     *
-	 * @param indice TODO
-	 * @return TODO
-	 */
-	public Piece getWhitePiece(int indice)
-	{
-		return white[indice];
 	}
 
 	/**
@@ -65,21 +37,48 @@ public class Board
      *
 	 * @return TODO
 	 */
-	private Piece[] createPieceGame()
+	private Piece[][] createPieceGame()
 	{
-        Piece[] pieces = new Piece[NUMBER_OF_PIECES];
-		pieces[0] = new King();
-		pieces[1] = new Queen();
-        pieces[2] = new Knight();
-        pieces[3] = new Knight();
-        pieces[4] = new Rook();
-        pieces[5] = new Rook();
-        pieces[6] = new Bishop();
-        pieces[7] = new Bishop();
-
-		for (int i = 8; i < 16; i++)
+      
+		Piece[][] pieces = new Piece[this.WIDTH][this.HEIGHT];
+		/**
+		 * pieces of one player
+		 */
+		pieces[0][0] = new Rook();
+		pieces[1][0] = new Knight();
+		pieces[2][0] = new Bishop();
+		pieces[3][0] = new Queen();
+		pieces[4][0] = new King();
+		pieces[5][0] = new Bishop();
+		pieces[6][0] = new Knight();
+		pieces[7][0] = new Rook();
+		for (int i = 0; i < 8; i++)
 		{
-			pieces[i] = new Pawn();
+			pieces[i][1] = new Pawn();
+		}
+		/**
+		 * pieces of the latter
+		 */
+		
+		pieces[0][7] = new Rook();
+		pieces[1][7] = new Knight();
+		pieces[2][7] = new Bishop();
+		pieces[3][7] = new Queen();
+		pieces[4][7] = new King();
+		pieces[5][7] = new Bishop();
+		pieces[6][7] = new Knight();
+		pieces[7][7] = new Rook();
+		for (int i = 0; i < 8; i++)
+		{
+			pieces[i][6] = new Pawn();
+		}
+		
+		for(int i = 2;i <5;i++)
+		{
+			for(int j = 0;j<8;j++)
+			{
+				pieces[j][i] = new Blank();
+			}
 		}
 
 		return pieces;
---
+++ src/fr/iutvalence/henrionjulien/chess/piece/Blank.java
@@ -0,0 +1,19 @@
+package fr.iutvalence.henrionjulien.chess.piece;
+/**
+ * this class is use for simulate a blank case of the board.
+ * @author henrionj
+ *
+ */
+public class Blank extends Piece {
+	
+	public Blank()
+	{
+		super();
+	}
+	@Override
+	protected int[][] createMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
